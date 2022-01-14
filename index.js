ec5ce5c1a2084dba80b237e9424d54f2ad3f6449
unknown <byoo@fasoo.com>
2022-01-14 13:07:30 +0900

    default upload server

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+const app = express();
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, __dirname);
+  },
+  filename: (req, file, callback) => {
+    callback(null, file.originalname);
+  },
+});
+const uploader = multer({ storage: storage });
+
+console.log(path.dirname);
+
+app.get("/", (req, res) => {
+  console.log(req);
+  res.status(200).send("hihi");
+});
+
+app.post("/", uploader.any(), (req, res) => {
+  console.log(req);
+  res.status(200).send({ test: "ok" });
+});
+
+app.listen(8080, () => {
+  console.log("this server is running!");
+});
